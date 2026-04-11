6ce9042c20f3dfa1aa9a37b75bb55ec7cc13caa6
AmgadEmbaby <amgad.ahmed.ismail.1451@gmail.com>
2026-04-11 22:54:17 +0200

    Guests class attributes only will be edited later

---
+++ src/Guests.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Guests {
+
+    private String userName;
+    private String passWord;
+    private Date dateOfBirth;
+    private double Balance;
+    private String adress;
+    //Enum For gender lama azkro hader wallahi
+    //Room Prefrence lama n3ml el Room class
+
+
+
+}
