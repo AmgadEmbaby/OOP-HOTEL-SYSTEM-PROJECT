bca384abc747df32dbcd5c3e851922ed95173743
AmgadEmbaby <amgad.ahmed.ismail.1451@gmail.com>
2026-04-11 23:02:50 +0200

    Guests class attributes only will be edited later

--- src/Guests.java
+++ src/Guests.java
@@ -11,5 +11,43 @@ public class Guests {
     //Room Prefrence lama n3ml el Room class
 
 
+    public String getUserName() {
+        return userName;
+    }
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public double getBalance() {
+        return Balance;
+    }
+
+    public void setBalance(double balance) {
+        Balance = balance;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
 }
