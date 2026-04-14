9b1aa11ccdf6cd265c8e9018346ac4aea36ff3b6
AmgadEmbaby <amgad.ahmed.ismail.1451@gmail.com>
2026-04-14 19:56:42 +0200

    Guests class attributes, setters and getters , constructors done , methods are work in progress , Enum done REGISTER FEATUER IS A WORK IN PROGRESS
    ROOM WAITING FOR ROOM CLASS TO BE DONE

--- src/Guests.java
+++ src/Guests.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.Scanner;
 
 public class Guests {
 
@@ -6,11 +7,11 @@ public class Guests {
     private String passWord;
     private Date dateOfBirth;
     private double Balance;
-    private String adress;
+    private String address;
     private Gender gender;
     //Room Prefrence lama n3ml el Room class
 
-
+    Scanner input = new Scanner(System.in);
 
 
     enum Gender{
@@ -18,9 +19,9 @@ public class Guests {
 
     }
 
-    public Guests(String userName, String adress, double balance, Date dateOfBirth, String passWord,Gender gender) {
+    public Guests(String userName, String address, double balance, Date dateOfBirth, String passWord, Gender gender) {
         this.userName = userName;
-        this.adress = adress;
+        this.address = address;
         Balance = balance;
         this.dateOfBirth = dateOfBirth;
         this.passWord = passWord;
@@ -63,12 +64,12 @@ public class Guests {
         Balance = balance;
     }
 
-    public String getAdress() {
-        return adress;
+    public String getAddress() {
+        return address;
     }
 
-    public void setAdress(String adress) {
-        this.adress = adress;
+    public void setAddress(String address) {
+        this.address = address;
     }
     public Gender getGender() {
         return gender;
@@ -77,4 +78,34 @@ public class Guests {
     public void setGender(Gender gender) {
         this.gender = gender;
     }
+
+    public void Register(){
+        Guests.Gender tempGender = null;
+        System.out.println("Welcome to the our hotels app!!! ");
+        System.out.print("Please enter your desired username: ");
+        System.out.print("Enter your gender: ");
+        String InputTempGender = input.nextLine();
+        if(InputTempGender.equalsIgnoreCase("male")){
+            tempGender = Guests.Gender.male;
+        }
+        else{
+            tempGender = Guests.Gender.female;
+        }
+        // VALIDATION FOR INPUT HERE TO BE USED LATER
+       String tempUsername =input.nextLine();
+        System.out.print("Enter your date of birth: ");
+       // String tempDOB = input.next
+        System.out.print("Enter your password: ");
+        String tempPassword = input.nextLine();
+        System.out.print("Enter your address: ");
+        String tempAddress = input.nextLine();
+        System.out.print("Enter your balance: ");
+        int tempBalance = input.nextInt();
+
+        //Guests guests = new Guests(tempUsername,tempAddress,tempBalance,tempDOB,tempPassword,tempGender);
+        //public Guests(String userName, String address, double balance, Date dateOfBirth, String passWord, Gender gender)
+
+
+
+
 }
