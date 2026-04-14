caa8abcc50618b51e0c0fa6d2989a32f75837c92
Mariam Sherief <mariamsherief197@gmail.com>
2026-04-15 00:03:37 +0200

    All amenities that might be in the room with their prices.

---
+++ src/Amenity.java
@@ -0,0 +1,45 @@
+public class Amenity {
+
+    private boolean wifi=false;
+    private boolean housekeeping=false;
+    private boolean LaundryService =false;
+    private boolean minibar=false;
+    private boolean RoomDigitalSafe =false;
+    private boolean BreakfastInBed =false;
+    private boolean AddExtraBed=false;
+    private boolean ExtraPillows=false;
+    private boolean ExtraToiletries=false;
+    private boolean ExtraTowels=false;
+    private boolean BathSet=false; //robe&slippers
+    private boolean SewingKit=false;
+    private boolean CoffeeMachine=false;
+    private boolean BluetoothSpeaker=false;
+    private boolean jacuzzi =false;
+    private boolean spa=false;
+    private boolean PrivatePool=false;
+    private boolean LateCheckOut=false;
+
+
+    //all prices are in dollars($)
+    public static double WifiCost=10;
+    public static double HousekeepingCost=25;
+    public static double LaundryCost=15;
+    public static double MinibarCost=25;
+    public static double SafeCost=20;
+    public static double BreakfastInBedCost=40;
+    public static double ExtraBedCost=30;
+    public static double ExtraPillowsCost=8;
+    public static double ExtraToiletriesCost=10;
+    public static double ExtraTowelsCost=8;
+    public static double BathSetCost=12;
+    public static double SewingKitCost=5;
+    public static double CoffeeMachineCost=24;
+    public static double BluetoothSpeakerCost=7;
+    public static double JacuzziCost=135;
+    public static double SpaCost=75;
+    public static double PrivatePoolCost=375;
+    public static double LateCheckOutCost=15;
+
+    public Amenity() {
+    }
+}
