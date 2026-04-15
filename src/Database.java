4d0189f9a00540bf65a15e80406c78fb03bf5ace
HallaReda <hallareda2008@gmail.com>
2026-04-16 00:02:13 +0200

    updated database

--- src/Database.java
+++ src/Database.java
@@ -7,10 +7,12 @@
 import java.util.*;
 
 public class Database {
+
   private static  ArrayList<Guests> guestList= new ArrayList<>();
 
   private static  ArrayList<Rooms> roomList= new ArrayList<>();
 
+  private static  ArrayList<Reservations> reservationsList= new ArrayList<>();
 
 
 
@@ -21,4 +23,15 @@ public class Database {
   public static ArrayList<Rooms> getRoomList(){
     return roomList;
   }
+
+
+  public static ArrayList<Reservations> getReservationsList(){
+
+    return reservationsList;
+}
+
+
+
+
+
 }
