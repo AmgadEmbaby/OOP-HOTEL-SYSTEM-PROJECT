7cd1621fe47dd0dfa40cd6610f52af1c9ced2b22
HallaReda <hallareda2008@gmail.com>
2026-04-15 03:09:47 +0200

    implemented Staff attributes

--- src/Staff.java
+++ src/Staff.java
@@ -1,3 +1,17 @@
-public class Staff {
+
+import java.time.*;
+
+public abstract class Staff {
+
     enum Role {ADMIN, RECEPTIONIST}
+    private String userName;
+    private String passWord;
+    private LocalDate dateOfBirth;
+    private Role role;
+    private int workingHours;
+
+
+
+
+
 }
