d8636da1c529b292ba1f703301fbf02885273ff9
HallaReda <hallareda2008@gmail.com>
2026-04-16 02:27:37 +0200

    updated Admin class

--- src/Admin.java
+++ src/Admin.java
@@ -1,3 +1,28 @@
 public class Admin extends Staff{
-    //CRUD
+
+    //create
+    public void addRoom(Rooms room){
+        Database.getRoomList().add(room);
+    }    //the room is created in the main and added only by the admin
+
+    //helper func
+    public Rooms findRoom(int roomNumber){
+        for(Rooms r: Database.getRoomList()){
+            if(r.getRoomNumber()==roomNumber ){
+                return r;
+            }
+        }
+        return null;
+    }
+
+    //update functions
+    public void updatePrice(Rooms room, int price, int roomNumber){
+      findRoom(roomNumber);
+
+    }
+
+
+
+
+
 }
