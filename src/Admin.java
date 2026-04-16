cfd0a8a15d07b30c688b8aadc907d502151c7108
HallaReda <hallareda2008@gmail.com>
2026-04-17 01:44:09 +0200

    completed amenities CRUD

--- src/Admin.java
+++ src/Admin.java
@@ -1,5 +1,5 @@
 public class Admin extends Staff{
-
+ //------------------------- create,update,delete functions for rooms------------------
     //create
     public void addRoom(Rooms room){
         Database.getRoomList().add(room);
@@ -27,6 +27,46 @@ public class Admin extends Staff{
       Rooms room= findRoom(roomNumber);
      //SHOULD I ADD I ROOM DOESNT EQUAL NULL??
         Database.getRoomList().remove(room);
+    }
+
+    //------------------------- create,update,delete functions for amenities ------------------
+    //create
+    public void addAmenity(Amenity amenity){
+        Database.getamenitiesList().add(amenity);
+    }
+
+    //helper
+    public Amenity findAmenity(String amenityName)
+    {
+        for(Amenity a: Database.getamenitiesList()){
+            if(amenityName.equalsIgnoreCase(a.getAmenityName()) ){
+                return a;
+            }
+        }
+        return null;
+    }
+
+    //update
+    public void updateAmenityCost (int price, String amenityName)
+    {
+        Amenity amenity= findAmenity(amenityName);
+        amenity.setAmenityCost(price);
+
+    }
+
+    public void updateAmenityAvailability (boolean availability, String amenityName)
+    {
+        Amenity amenity= findAmenity(amenityName);
+        amenity.setAvailable(availability);
+
+    }
+
+    //delete
+    public void deleteAmenity ( String amenityName)
+    {
+        Amenity amenity= findAmenity(amenityName);
+        Database.getamenitiesList().remove(amenity);
+
     }
 
 
