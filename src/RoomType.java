383e582b3e25a910b8c445e58105df7596e8940f
AmgadEmbaby <amgad.ahmed.ismail.1451@gmail.com>
2026-04-16 03:24:22 +0200

    FIXED SOMEBUGS IN THE Rooms CLASS DUMMY DATA FOR ROOMS

--- src/RoomType.java
+++ src/RoomType.java
@@ -4,6 +4,37 @@ private int NumberOfBeds;
 private int Capacity;
 private String RoomDescription;
 private double PricePerNight;
+private double singlePrice =70;
+private double doublePrice =125;
+private double suitePrice=465;
+
+
+
+
+
+    public double getSinglePrice() {
+        return singlePrice;
+    }
+
+    public void setSinglePrice(double singlePrice) {
+        this.singlePrice = singlePrice;
+    }
+
+    public double getSuitePrice() {
+        return suitePrice;
+    }
+
+    public void setSuitePrice(double suitePrice) {
+        this.suitePrice = suitePrice;
+    }
+
+    public double getDoublePrice() {
+        return doublePrice;
+    }
+
+    public void setDoublePrice(double doublePrice) {
+        this.doublePrice = doublePrice;
+    }
 
     public String getTypeName() {
         return TypeName;
@@ -54,21 +85,21 @@ private double PricePerNight;
        if(TypeName.equalsIgnoreCase("single")){
           this.NumberOfBeds=1;
           this.Capacity=1;
-          this.PricePerNight=70; //prices in dollars($)
+          this.PricePerNight=singlePrice; //prices in dollars($)
            this.RoomDescription="A room designed for one guest, it offers a small but comfortable space," +
                    "it's perfect for solo travellers like business guests or short stays.";
        }
        if(TypeName.equalsIgnoreCase("double")){
            this.NumberOfBeds=2;
            this.Capacity=2;
-           this.PricePerNight=125;
+           this.PricePerNight=doublePrice;
            this.RoomDescription="A room more spacious than the single and designed for two guest, you can change" +
                    "the two beds with one king size, it's perfect for friends or couple travelling together.";
        }
        if(TypeName.equalsIgnoreCase("suite")){
            this.NumberOfBeds=1;
            this.Capacity=4;
-           this.PricePerNight=465;
+           this.PricePerNight=doublePrice;
            this.RoomDescription="A large and luxurious room , it has s separate living area with a sofa bed " +
                    ",bedroom with a large king size bed and a small kitchen with a mini bar," +
                    "it's perfect for a small family or guests who want to have a private and luxurious stay.";
