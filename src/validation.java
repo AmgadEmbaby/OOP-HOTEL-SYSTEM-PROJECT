af9db166c8b6d0e855e32b968a34f79c0921ec32
hanas <hanashaker8@gmail.com>
2026-04-16 05:47:25 +0200

    Validation branch only

--- src/validation.java
+++ src/validation.java
@@ -1,4 +1,177 @@
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class validation {
+
+        String[] existingUsers; // EDIT LAMA ARRAY YET3EMEL
+
+
+
+        static String validUsername(String[] existingUsers){
+// edit array name 7asab your array name
+
+            Scanner input = new Scanner(System.in);
+            String userName;
+
+            while(true) {
+                System.out.print("Enter username: ");
+                userName = input.nextLine();
+
+                //not empty
+                if (userName == null || userName.trim().isEmpty()) {
+                    System.out.print("Invalid: empty username");
+                    continue;
+                }
+
+                //alphanumeric
+                boolean validAlphanumeric = true;
+                for (int i = 0; i < userName.length(); i++) {
+                    char c = userName.charAt(i);
+                    if (!Character.isLetterOrDigit(c)) {
+                        validAlphanumeric = false;
+                        break; // so it doesnt ignore one wrong character only
+                    }
+                }
+
+                if(!validAlphanumeric){
+                    System.out.println("Invalid: only letters and numbers allowed");
+                    continue;
+                }
+
+
+                //no duplicates
+                boolean validNotDuplicate = true;
+                for (int i = 0; i < existingUsers.length; i++) {
+                    if (userName.equals(existingUsers[i])) { //if found in array
+                        validNotDuplicate = false;
+                        break;
+                    }
+                }
+                if (!validNotDuplicate){
+                    System.out.println("Invalid: username already exists");
+                    continue;
+                }
+
+                return userName;
+            }
+        }
+
+
+
+
+        public static String validPassword(){
+
+            //mot empty, no less than 8
+
+            Scanner input = new Scanner(System.in);
+            String passWord;
+
+            while (true) {
+                System.out.print("Enter password: ");
+                passWord = input.nextLine();
+
+                if (passWord == null || passWord.length() < 8) {
+                    System.out.println("Invalid password, minimum 8 characters");
+                    continue;
+                }
+                return passWord;
+            }
+        }
+
+
+
+
+   public static LocalDate validBirthdate() {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter year: ");
+            int year = input.nextInt();
+
+            System.out.print("Enter month: ");
+            int month = input.nextInt();
+
+            System.out.print("Enter day: ");
+            int day = input.nextInt();
+
+            try {
+                LocalDate dateOfBirth = LocalDate.of(year, month, day);
+
+                if (dateOfBirth.isAfter(LocalDate.now())) {
+                    System.out.println("BirthDate you selected is in future. Please try again.");
+                    continue;
+                }
+
+                return dateOfBirth;
+            }
+            catch(Exception datenonexistent){
+                System.out.println("Invalid date. Try again.");
+            }
+        }
+    }
+
+
+
+
+     public static Guests.Gender validGender(){
+                return gender!=null; // if gender = null returns false
+    }
+    // try catch
+
+
+
+
+
+    public static String validAddress (){
+            Scanner input = new Scanner(System.in);
+            String address;
+
+            while(true){
+                System.out.print("enter address: ");
+                address = input.nextLine();
+
+                if(address == null || address.trim().isEmpty() ){
+                    System.out.println("\"Invalid address. Please enter valid address.\"");
+                    continue;
+                }
+
+                return address;
+            }
+    }
+
+
+
+
+
+    public static double validBalance (){
+           Scanner input = new Scanner(System.in);
+           double balance;
+
+           while(true){
+               System.out.print("Enter balance: ");
+
+               if (!input.hasNextDouble()) { //check for letter prevent crash
+                   System.out.println("Invalid input. Enter a number.");
+                   input.next(); // clearing prev input
+                   continue;
+               }
+
+
+               balance = input.nextDouble();
+
+               if(balance <= 0 ){
+                   System.out.println("Balance is too low. Please enter valid balance.");
+                   continue;
+               }
+
+               return balance;
+
+           }
+
+    }
+
+
 
 
 
