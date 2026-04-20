2af1d22bbe60b770588718fe84b62203a20547a5
hanas <hanashaker8@gmail.com>
2026-04-20 02:28:01 +0200

    room validation in progress

---
+++ src/RoomValidation.java
@@ -0,0 +1,23 @@
+public class RoomValidation {
+
+//public static RoomType validateRoomTypeName(String roomtype){
+//    Validator.checkStringNotEmpty(roomtype, "Room type");
+//
+//    String r = roomtype.toLowerCase().trim();
+//
+//    MAKE IT BASED ON LIST NOT FINAL STRING
+//
+//    if (r.equals("single") ||
+//            r.equals("double") ||
+//            r.equals("suite")) {
+//
+//    }
+//
+//    return r;
+//}
+
+
+//public static{}
+
+
+}
