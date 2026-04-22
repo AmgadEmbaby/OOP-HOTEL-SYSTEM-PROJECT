3b0f74424adaece220c13310fb065483e5e914d6
hanas <hanashaker8@gmail.com>
2026-04-22 19:29:29 +0200

    Reservation validation after merge

--- src/Reservations.java
+++ src/Reservations.java
@@ -10,10 +10,14 @@ public class Reservations {
     private int reservationID;
     private Invoices.PaymentMethod method;
 
-    public Reservations(Guests guest, RoomType roomType, LocalDate in, LocalDate out,Invoices.PaymentMethod method) throws Exception {
-        if (!out.isAfter(in)) {
-            throw new Exception("Check-out must be after check-in.");
-        }
+    public Reservations(Guests guest, RoomType roomType, LocalDate in, LocalDate out,Invoices.PaymentMethod method){
+//            throws Exception {
+//        if (!out.isAfter(in)) {
+//            throw new Exception("Check-out must be after check-in.");
+//        }
+
+        ReservationsValidation.validateReservation(guest, in, out);
+
         this.typeDesired= roomType;
         this.reservationID=idCounter++;
         this.guest = guest;
@@ -36,8 +40,12 @@ public class Reservations {
     }
 
     public void cancelReservation() {
+        ReservationsValidation.validateCancellation(guest, this);
         this.status = ReservationStatus.CANCELLED;
-        this.room.setStatus(Rooms.RoomStatus.AVAILABLE);
+
+        if (this.room != null) {
+            this.room.setStatus(Rooms.RoomStatus.AVAILABLE);
+        }
     }
 
     public void checkInGuest() {
@@ -53,6 +61,7 @@ public class Reservations {
     }
 
     public void setRoom(Rooms room) {
+        ReservationsValidation.validateRoomAssignment(room, this.checkin, this.checkout);
         this.room = room;
     }
 
