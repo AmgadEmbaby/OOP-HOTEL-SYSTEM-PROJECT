c46daebbef837c5df22b592d30dd69ce235c7d48
hanas <hanashaker8@gmail.com>
2026-05-09 02:09:51 +0300

    ,,

--- src/RegisterController.java
+++ src/RegisterController.java
@@ -63,10 +63,10 @@ public class RegisterController {
 
             guest.setBalance(balance);
 
-            // YOUR VALIDATION
+
             GuestValidation.validateRegister(guest);
 
-            // ADD TO DATABASE
+
             Database.addGuests(guest);
 
             errorLabel.setStyle("-fx-text-fill: #b8d6b8;");
